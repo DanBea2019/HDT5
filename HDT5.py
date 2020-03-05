dba5b85c8482425e48933a4d96b4eef0f4a6f836
DanBea2019 <53918887+DanBea2019@users.noreply.github.com>
2020-03-04 23:24:52 -0600

    Update HDT5.py

--- HDT5.py
+++ HDT5.py
@@ -7,15 +7,33 @@ import random
 #new
 #1=ocupado
 #0=libre
-def tiempo(num):
+def espera(numero,env,memoria):
     puestos=[0,0,0,0,0,0,0,0,0,0]
     puesto1=[]
-    num=random.random(0,10)
-    for elemento in puestos:
-        elemento = 1
-    puesto1.append(elemento)
-    print ("Su numero es: ", num)
-    if puesto1 == 1:
-        print ("Pasara al sigueinte")
-    elif puesto1 == 0:
-        print ("Espere esta ocupado")
+    RAM = simpy.Container(env, init=100, capacity=100)
+
+    RAM: RAM.get(memoria)
+
+    RAM: RAM.put(memoria)
+    while True:
+        numero = random.randint(0,10)
+        for elemento in puestos:
+            elemento = 1
+            puesto1.append(elemento)
+            print ("Su numero es: ", num)
+        if puesto1 == 1:
+            print ("Pasara al sigueinte")
+        elif puesto1 == 0:
+            print ("Espere esta ocupado")
+ 
+        while memoria <= 100:
+            
+#
+env = simpy.Environment() #crear ambiente de simulacion
+bcs = simpy.Resource(env, capacity=10)
+
+#crear
+for i in range(1):
+    env.process(espera(env, 'Car %d' % i, bcs, i*10, 1))
+#correr el programa
+env.run()
